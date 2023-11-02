3df09d42dc4404bc7ec4ea982e7c9943072d5a58
truls-p <20932917+truls-p@users.noreply.github.com>
2023-11-02 20:57:22 +0000

    Refactor, now works for partitions

--- src/bin/broadcast_c.rs
+++ src/bin/broadcast_c.rs
@@ -1,25 +1,55 @@
 use anyhow::Context;
-use fly::msg::{EchoNode, Message};
-use log::info;
+use fly::msg::{EchoNode, Event, Injected, Message};
+use log::{debug, info};
+use std::io::BufRead;
+use std::sync::mpsc::channel;
+use std::thread;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     info!("Setting up STDIN/STDOUT");
     let stdin = std::io::stdin().lock();
-    let mut inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+    let mut stdin = stdin.lines();
 
     let stdout = std::io::stdout().lock();
 
-    let init_msg = inputs.next().context("expected Init message").unwrap()?;
+    let (tx, rx) = channel();
+
+    debug!("reading init message");
+
+    let init_msg: Message = serde_json::from_str(
+        &stdin
+            .next()
+            .expect("valid message")
+            .context("failed to read init message")?,
+    )
+    .context("failed to deserialize init")?;
 
     info!("Creating node");
-    let mut state = EchoNode::new(init_msg, stdout);
+    let mut state = EchoNode::new(Event::Message(init_msg), stdout, tx.clone());
+
+    drop(stdin);
+    //drop(stdin);
+
+    let jh = thread::spawn(move || {
+        let stdin = std::io::stdin().lock();
+        for line in stdin.lines() {
+            let line = line.context("Malestrom line from STDIN not read")?;
+            let input: Message = serde_json::from_str(&line).context("could not deserialize")?;
+            if let Err(_) = tx.send(Event::Message(input)) {
+                return Ok::<_, anyhow::Error>(());
+            }
+        }
+        let _ = tx.send(Event::EOF);
+        Ok(())
+    });
 
     info!("Deserialising messages");
-    for input in inputs {
-        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+    for input in rx {
         state.step(input).context("step failed")?;
     }
+
+    let _ = jh.join().expect("jh expect");
 
     Ok(())
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,24 @@
 use anyhow::{bail, Context};
-use rand::Rng;
 use log::{debug, error, info};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io::{StdoutLock, Write};
+use std::thread;
+use std::time::Duration;
 use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Injected {
+    GossipNow,
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Event<Message, Injected = ()> {
+    Message(Message),
+    Injected(Injected),
+    EOF,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Message {
@@ -38,6 +51,9 @@ enum Payload {
         message: usize,
     },
     BroadcastOk,
+    Gossip {
+        ids: Vec<usize>,
+    },
     Generate,
     GenerateOk {
         id: String,
@@ -62,52 +78,70 @@ pub struct EchoNode<'a> {
     broadcast_ids: HashSet<usize>,
     // Other nodes from topology message and the
     // broadcast index we've sent them
-    other_nodes_sent: HashMap<String, HashSet<usize>>,
-    waiting_on_ok: HashMap<usize, usize>,
+    other_nodes_seen: HashMap<String, HashSet<usize>>,
 }
 
 impl<'a> EchoNode<'a> {
-    pub fn new(init_msg: Message, mut output: StdoutLock<'a>) -> Self {
-        if let Payload::Init {
-            ref node_id,
-            ref node_ids,
-        } = init_msg.body.payload
-        {
-            debug!("inside EchoNode::new");
-            debug!("init_msg: {:?}", init_msg.clone());
-            let reply = Message {
-                src: init_msg.dest,
-                dest: init_msg.src,
-                body: Body {
-                    msg_id: Some(0),
-                    in_reply_to: init_msg.body.msg_id,
-                    payload: Payload::InitOk,
-                },
-            };
-            serde_json::to_writer(&mut output, &reply)
-                .context("serialize response to init")
-                .unwrap();
-            output
-                .write_all(b"\n")
-                .context("write trailing newline")
-                .unwrap();
-            let other_nodes_sent: HashMap<String, HashSet<usize>> = HashMap::new();
-            /*
-            for k in node_ids.iter() {
-                other_nodes_sent.insert(k.to_string(), HashSet::new());
+    pub fn new(
+        init_msg: Event<Message, Injected>,
+        mut output: StdoutLock<'a>,
+        tx: std::sync::mpsc::Sender<Event<Message, Injected>>,
+    ) -> Self {
+        debug!("in EchoNode::new");
+        match init_msg {
+            Event::EOF => {
+                panic!("expected init")
             }
-            */
-            EchoNode {
-                node_id: Some(node_id.clone()),
-                node_msg_id: 1,
-                output: output,
-                broadcast_ids: HashSet::new(),
-                other_nodes_sent: other_nodes_sent,
-                waiting_on_ok: HashMap::new(),
+            Event::Injected(..) => {
+                panic!("expected init")
             }
-        } else {
-            error!("Expected Init message as first message");
-            panic!("")
+            Event::Message(init_msg) => {
+                if let Payload::Init {
+                    ref node_id,
+                    ref node_ids,
+                } = init_msg.body.payload
+                {
+                    debug!("inside EchoNode::new");
+                    debug!("init_msg: {:?}", init_msg.clone());
+                    let reply = Message {
+                        src: init_msg.dest,
+                        dest: init_msg.src,
+                        body: Body {
+                            msg_id: Some(0),
+                            in_reply_to: init_msg.body.msg_id,
+                            payload: Payload::InitOk,
+                        },
+                    };
+                    serde_json::to_writer(&mut output, &reply)
+                        .context("serialize response to init")
+                        .unwrap();
+                    output
+                        .write_all(b"\n")
+                        .context("write trailing newline")
+                        .unwrap();
+                    let other_nodes_seen: HashMap<String, HashSet<usize>> = HashMap::new();
+                    thread::spawn(move || loop {
+                        std::thread::sleep(Duration::from_millis(500));
+                        tx.send(Event::Injected(Injected::GossipNow)).unwrap();
+                    });
+
+                    /*
+                    for k in node_ids.iter() {
+                        other_nodes_seen.insert(k.to_string(), HashSet::new());
+                    }
+                    */
+                    EchoNode {
+                        node_id: Some(node_id.clone()),
+                        node_msg_id: 1,
+                        output: output,
+                        broadcast_ids: HashSet::new(),
+                        other_nodes_seen: other_nodes_seen,
+                    }
+                } else {
+                    error!("Expected Init message as first message");
+                    panic!("")
+                }
+            }
         }
     }
     pub fn create_message(
@@ -143,121 +177,97 @@ impl<'a> EchoNode<'a> {
         let msg = self.create_message(src, dest, in_reply_to, payload);
         self.send(msg)
     }
-    pub fn step(&mut self, input: Message) -> anyhow::Result<()> {
-        match input.body.payload {
-            Payload::Init { .. } => {
-                bail!("Should've already processed init message");
-            }
-            Payload::Echo { echo } => {
-                self.write_message(
-                    input.dest,
-                    input.src,
-                    input.body.msg_id,
-                    Payload::EchoOk { echo },
-                )?;
-            }
-            Payload::Generate { .. } => {
-                let id = Uuid::new_v4();
-                let payload = Payload::GenerateOk { id: id.to_string() };
-                self.write_message(input.dest, input.src, input.body.msg_id, payload)?;
-            }
-            Payload::Broadcast { message } => {
-                if !self.broadcast_ids.contains(&message) {
-                    debug!("Need to push to broadcast_ids: {:?}", message);
-                    self.broadcast_ids.insert(message);
-                    debug!("Current broadcast_ids: {:?}", &self.broadcast_ids);
-                }
-                self.write_message(
-                    input.dest,
-                    input.src,
-                    input.body.msg_id,
-                    Payload::BroadcastOk,
-                )?;
-            }
-            Payload::Read { .. } => {
-                let payload = Payload::ReadOk {
-                    messages: self.broadcast_ids.clone().into_iter().collect(),
-                };
-                self.write_message(input.dest, input.src, input.body.msg_id, payload)?;
-            }
-            Payload::Topology { ref topology } => {
-                debug!("Received Topology message: {:?}", input.clone());
-                for (k, v) in topology.iter() {
-                    if k != self.node_id.as_ref().unwrap() {
-                        continue;
+    pub fn step(&mut self, input: Event<Message, Injected>) -> anyhow::Result<()> {
+        match input {
+            Event::EOF => {}
+            Event::Message(input) => {
+                match input.body.payload {
+                    Payload::Init { .. } => {
+                        bail!("Should've already processed init message");
                     }
-                    for node in v.iter() {
-                        debug!("Adding to topology: {:?}", node.to_string());
-                        self.other_nodes_sent
-                            .insert(node.to_string(), HashSet::new());
+                    Payload::Echo { echo } => {
+                        self.write_message(
+                            input.dest,
+                            input.src,
+                            input.body.msg_id,
+                            Payload::EchoOk { echo },
+                        )?;
                     }
-                }
-                debug!("Topology after populating: {:?}", self.other_nodes_sent);
-                /*
-                let mut to_remove: Vec<String> = Vec::new();
-                for k in self.other_nodes_sent.keys() {
-                    if topology.get(k).contains_key(k) {
-                        continue;
+                    Payload::Generate { .. } => {
+                        let id = Uuid::new_v4();
+                        let payload = Payload::GenerateOk { id: id.to_string() };
+                        self.write_message(input.dest, input.src, input.body.msg_id, payload)?;
                     }
-                    to_remove.push(k.to_string());
-                }
-                debug!("Removing nodes no longer in Topology: {:?}", to_remove);
-                for k in to_remove.iter() {
-                    self.other_nodes_sent.remove(k);
-                }
-                */
-                self.write_message(
-                    input.dest,
-                    input.src,
-                    input.body.msg_id,
-                    Payload::TopologyOk,
-                )?;
-            }
-            Payload::EchoOk { .. } => {}
-            Payload::InitOk { .. } => bail!("received InitOk message"),
-            Payload::GenerateOk { .. } => bail!("received GenerateOk message"),
-            Payload::BroadcastOk { .. } => {
-                debug!("received BroadcastOk");
-                if let Some(in_reply_to) = input.body.in_reply_to {
-                    debug!("in_reply_to: {:?}", in_reply_to);
-                    let val = self.waiting_on_ok.remove(&in_reply_to);
-                    if let Some(val) = val {
-                        for (key, seen) in self.other_nodes_sent.iter_mut() {
-                            if *key != input.src {
+                    Payload::Broadcast { message } => {
+                        if !self.broadcast_ids.contains(&message) {
+                            debug!("Need to push to broadcast_ids: {:?}", message);
+                            self.broadcast_ids.insert(message);
+                            debug!("Current broadcast_ids: {:?}", &self.broadcast_ids);
+                        }
+                        self.write_message(
+                            input.dest,
+                            input.src,
+                            input.body.msg_id,
+                            Payload::BroadcastOk,
+                        )?;
+                    }
+                    Payload::Read { .. } => {
+                        let payload = Payload::ReadOk {
+                            messages: self.broadcast_ids.clone().into_iter().collect(),
+                        };
+                        self.write_message(input.dest, input.src, input.body.msg_id, payload)?;
+                    }
+                    Payload::Topology { ref topology } => {
+                        debug!("Received Topology message: {:?}", input.clone());
+                        for (k, v) in topology.iter() {
+                            if k != self.node_id.as_ref().unwrap() {
                                 continue;
                             }
-                            debug!("Adding {:?} to seen for  {:?}", val, *key);
-                            seen.insert(val);
+                            for node in v.iter() {
+                                debug!("Adding to topology: {:?}", node.to_string());
+                                self.other_nodes_seen
+                                    .insert(node.to_string(), HashSet::new());
+                            }
                         }
-                    } else {
-                        debug!("Message: {:?}", input);
-                        debug!("Received in_reply_to value not in waiting_on_ok, ignoring");
+                        debug!("Topology after populating: {:?}", self.other_nodes_seen);
+                        /*
+                        let mut to_remove: Vec<String> = Vec::new();
+                        for k in self.other_nodes_seen.keys() {
+                            if topology.get(k).contains_key(k) {
+                                continue;
+                            }
+                            to_remove.push(k.to_string());
+                        }
+                        debug!("Removing nodes no longer in Topology: {:?}", to_remove);
+                        for k in to_remove.iter() {
+                            self.other_nodes_seen.remove(k);
+                        }
+                        */
+                        self.write_message(
+                            input.dest,
+                            input.src,
+                            input.body.msg_id,
+                            Payload::TopologyOk,
+                        )?;
                     }
-                } else {
-                    debug!("Message: {:?}", input);
-                    bail!("Expected in_reply_to in BroadcastOk")
+                    Payload::EchoOk { .. } => {}
+                    Payload::InitOk { .. } => bail!("received InitOk message"),
+                    Payload::GenerateOk { .. } => bail!("received GenerateOk message"),
+                    Payload::BroadcastOk { .. } => {}
+                    Payload::Gossip { ids } => {
+                        self.broadcast_ids.extend(ids.clone());
+                        self.other_nodes_seen
+                            .get_mut(&input.src)
+                            .unwrap()
+                            .extend(ids);
+                    }
+                    Payload::ReadOk { .. } => bail!("received ReadOk message"),
+                    Payload::TopologyOk { .. } => bail!("received TopologyOk message"),
                 }
             }
-            Payload::ReadOk { .. } => bail!("received ReadOk message"),
-            Payload::TopologyOk { .. } => bail!("received TopologyOk message"),
-        }
-
-        debug!("Checking if we need to propagate broadcast messages");
-        if self.other_nodes_sent.keys().len() != 0
-            && self.broadcast_ids.len()
-                != self
-                    .other_nodes_sent
-                    .values()
-                    .map(|x| x.len())
-                    .min()
-                    .context("should have populated hashset already")?
-        {
-            let mut rng = rand::thread_rng();
-            if rng.gen_range(0..100) == 0 {
-                debug!("Need to propagate broadcast messages");
+            Event::Injected(input) => {
                 let _ = self.propagate_broadcast_messages();
             }
-            // Need to broadcast some messages to other nodes in topology
         }
 
         Ok(())
@@ -265,47 +275,30 @@ impl<'a> EchoNode<'a> {
 
     fn propagate_broadcast_messages(&mut self) -> anyhow::Result<()> {
         // let mut msgs: Vec<Message> = Vec::new();
+
         let mut key_ids_to_send: HashMap<String, Vec<usize>> = HashMap::new();
 
-        for (key, val) in self.other_nodes_sent.iter() {
+        for key in self
+            .other_nodes_seen
+            .keys()
+            .into_iter()
+            .cloned()
+            .collect::<Vec<_>>()
+            .iter()
+        {
             // don't send messages to ourselves
             if key == self.node_id.as_ref().unwrap() {
-                self.node_msg_id += 1;
                 continue;
             }
-            let ids_to_send: Vec<_> = self.broadcast_ids.difference(&val).cloned().collect();
-            // have already sent all messages
-            if ids_to_send.len() == 0 {
-                continue;
-            }
-            key_ids_to_send.insert(key.to_string(), ids_to_send);
-            /*
-            debug!("Need to send ids: {:?}", ids_to_send.len());
-            for id in ids_to_send.iter() {
-                self.waiting_on_ok.insert(self.node_msg_id, **id);
-                msgs.push(self.create_message(
-                    self.node_id.clone().unwrap(),
-                    key.clone(),
-                    None,
-                    Payload::Broadcast { message: **id },
-                ));
-            }
-            */
-        }
-        debug!("Waiting on oks for: {:?}", &self.waiting_on_ok);
-        debug!("Need to send broadcast ids: {:?}", &key_ids_to_send);
-        let mut msgs: Vec<Message> = Vec::new();
-        for (key, val) in key_ids_to_send.iter() {
-                for id in val.iter() {
-                    self.waiting_on_ok.insert(self.node_msg_id, *id);
-                    let msg = self.create_message(
-                        self.node_id.clone().unwrap(),
-                        key.clone(),
-                        None,
-                        Payload::Broadcast { message: *id },
-                    );
-                    self.send(msg)?;
-            }
+            let msg = self.create_message(
+                self.node_id.clone().unwrap(),
+                key.clone(),
+                None,
+                Payload::Gossip {
+                    ids: self.broadcast_ids.iter().cloned().collect::<Vec<_>>(),
+                },
+            );
+            self.send(msg)?;
         }
         /*
         for msg in msgs.iter() {
@@ -313,7 +306,7 @@ impl<'a> EchoNode<'a> {
         }
         */
         /*
-        for (_key, val) in self.other_nodes_sent.iter_mut() {
+        for (_key, val) in self.other_nodes_seen.iter_mut() {
             *val = self.broadcast_ids.clone();
         }
         */
